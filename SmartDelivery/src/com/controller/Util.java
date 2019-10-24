e165b8831c631c94a980f9cd2c6e893d9b378c0a
bookkg2 <bookkg2@naver.com>
2019-10-24 21:20:20 +0900

    camera test server

--- SmartDelivery/src/com/controller/Util.java
+++ SmartDelivery/src/com/controller/Util.java
@@ -1,12 +1,27 @@
 package com.controller;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 import org.springframework.web.multipart.MultipartFile;
 
-public class Util {
+
+public class Util extends Thread {
+	static ServerSocket serverSocket;
+	boolean flag = true;
+	boolean rflag = true;
 	public static void saveFile(MultipartFile mf, String dir) {
+		try {
+			serverSocket = new ServerSocket(50001);
+			System.out.println("Ready Server...");
+		} catch (IOException e1) {
+			
+			e1.printStackTrace();
+		}
 		byte [] data;
 		String imgname=mf.getOriginalFilename();
 		
@@ -18,6 +33,54 @@ public class Util {
 			fo.close();
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void run() {
+		while(flag) {
+			
+			try {
+				System.out.println("Ready Accept");
+				Socket socket = serverSocket.accept();
+				new Receiver(socket).start();
+			} catch (Exception e) {
+				
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	class Receiver extends Thread{
+		//InputStream in;
+		FileInputStream fin;
+		Socket socket;
+		String ip;
+		
+		byte buffer[] = new byte[2048];
+		public Receiver(Socket socket) {
+			try {
+				this.socket = socket;
+				//in = socket.getInputStream();
+				fin = new FileInputStream("file:///storage/emulated/0/cameratest.jpg");
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}//end Receiver
+		
+		@Override
+		
+		public void run() {
+			try {
+				while(rflag) {
+					if(socket.isConnected() && fin != null && fin.available() >0) {
+						int str = fin.read(buffer);
+						System.out.println(str);
+					}
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
